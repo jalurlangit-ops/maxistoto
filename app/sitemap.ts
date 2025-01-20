94556c6a2590f5ab0a7ad5d51d668f66a91fb75f
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-20 22:35:20 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,22 @@
 import type { MetadataRoute } from 'next';
+import GlobalConst from "./var-global";
 
-const nameDomain = "https://www.bizworkit.com";
- 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `${nameDomain}`,
+      url: `${GlobalConst.domain}`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${nameDomain}`,
+      url: `${GlobalConst.domain}`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${nameDomain}`,
+      url: `${GlobalConst.domain}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
