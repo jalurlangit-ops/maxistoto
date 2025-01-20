192c15aa4a613b8b856099e5c3a491bedc552df6
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-20 21:09:38 +0700

    Update var-global.js

--- app/var-global.js
+++ app/var-global.js
@@ -1,1 +1,11 @@
-a
+module.exports = {
+    title: "Maxistoto : Bonus New Member 100 Di Awal Dan Situs Bonus New Member 100",
+    desc: "Maxistoto hadir sebagai situs terpercaya yang memberikan penawaran istimewa bagi para pemain baru! Dengan bonus new member 100% di awal, Anda bisa langsung menggandakan keseruan bermain tanpa perlu khawatir. Cukup lakukan deposit pertama Anda, dan nikmati tambahan saldo yang siap membawa Anda lebih dekat ke kemenangan besar",
+    keywords: "Maxistoto, Bonus New Member 100, Info Link Gacor, Link Gacor Hari Ini RTP Tinggi, Link Maxwin 2025, Info Link Slot Gacor, Link Slot Terpercaya Dan Gacor, Link Depo Gacor, Gacor77 Link Alternatif, Slot138 Login Link Alternatif",
+    brandName: "Maxistoto",
+    domain: "https://ensoftyazilim.com",
+    histats: '1,4921031,4,5,172,25,00010000',
+    gsc: 'eOaMJChOeLAWOTxBJC9KbWLvySruP7AJ4PYR0sgMpqE',
+    daftar: 'https://daftarwap.orang-dalam.link/maxis',
+    login: 'https://daftarwap.orang-dalam.link/maxis',
+}
